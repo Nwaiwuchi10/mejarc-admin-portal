c9f9daa1fb6c655c609e1cedf37db26cb0da7492
MAC <mac@MACs-MacBook-Pro.local>
2026-07-07 22:08:56 -0700

    base_url fix

--- src/Utils/Api.tsx
+++ src/Utils/Api.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:4500"
+  baseURL: "https://mejarc-backend.onrender.com"
   // `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
 });
 
